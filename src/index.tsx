52dca96a289b8bd01e036234c7f44128239b899d
ardhianfahmi <ardhianfahmi.sabani072@gmail.com>
2022-04-07 23:55:06 +0700

    .env 	src/Assets/ 	src/Auth/ 	src/Component/ 	src/Container/ 	src/Pages/ 	tslint.json

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { mount, route } from "navi";
+import { Router } from "react-navi";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import LoginPage from "./Auth/LoginPage";
+import RegisterPage from "./Auth/RegisterPage";
+import { Drawer } from "./Container/Drawer";
+import { TourTabs } from "./Container/Tourist";
+import { TouristCreateMain } from "./Container/TouristCreate";
+
+const routes = mount({
+  "/": route({
+    title: "Login",
+    view: <LoginPage />,
+  }),
+  "/home": route({
+    title: "login",
+    view: <Drawer />,
+  }),
+  "/tourist": route({
+    title: "login",
+    view: <TourTabs />,
+  }),
+  "/create": route({
+    title: "login",
+    view: <TouristCreateMain />,
+  }),
+  "/register": route({
+    title: "Register",
+    view: <RegisterPage />,
+  }),
+});
+
+ReactDOM.render(<Router routes={routes} />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
